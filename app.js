e62820a15b6d053c52d8c8a0eb44bc1f8cd92e3d
Glitch (sunrise-nasturtium-q5i7un8xvq) <none>
2020-01-18 09:39:06 +0000

    🚠📒 Updated with Glitch

---
+++ app.js
@@ -0,0 +1,190 @@
+/*
+ * Starter Project for Messenger Platform Quick Start Tutorial
+ *
+ * Remix this as the starting point for following the Messenger Platform
+ * quick start tutorial.
+ *
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ */
+
+"use strict";
+
+// Imports dependencies and set up http server
+const request = require("request"),
+  express = require("express"),
+  body_parser = require("body-parser"),
+  app = express().use(body_parser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+// Accepts GET requests at the /webhook endpoint
+app.get("/webhook", (req, res) => {
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = "bert";
+
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+// Accepts POST requests at /webhook endpoint
+app.post("/webhook", (req, res) => {
+  console.log("hallo123");
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === "page") {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      // Get the webhook event. entry.messaging is an array, but
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`
+    };
+
+    if (received_message.text.includes("test")) {
+      response = {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text: "What do you want to do next?",
+            buttons: [
+              {
+                "type": "postback",
+                "title": "Klik niet hier",
+                "payload": "KLIKNIETHIER"
+              }
+            ]
+          }
+        }
+      };
+    }
+  
+  // Sends the response message
+   if (received_message.text.includes("Repeat")) {
+     for(let i=0;i<20;i++){
+       setTimeout(()=>{
+         callSendAPI(sender_psid, {text:"Nummer "+i});
+       },500*i)
+     }
+    }else if(received_message.text.includes("start typen")){
+      callSenderActionApi(sender_psid,"typing_on")
+    }else{
+      callSendAPI(sender_psid, response);
+    }
+  }
+  
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  if(received_postback.payload=="KLIKNIETHIER"){
+    callSendAPI(sender_psid,{text:"Ik zei niet doen!"})
+  }
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid
+    },
+    message: response
+  };
+
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
+
+function callSenderActionApi(sender_psid, action){
+   var request_body={"recipient":{
+    "id":sender_psid
+  },
+  "sender_action":action
+}
+   /*
+   mark_seen = Mark last message as read
+
+typing_on = Turn typing indicators on
+
+typing_off = Turn typing indicators off
+*/
+    request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("action send");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
